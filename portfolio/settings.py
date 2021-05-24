3e65ba71c2c6ff49104d08c856b2fd524de6da66
Brayoh <nderitub30@gmail.com>
2021-05-24 12:26:31 +0300

    allow host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-h&^%(+68if-6g$04tciztdhh1^av9bcx_n_l$23!!85ed^51*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-two.herokuapp.com', '127.0.0.1' ]
 
 
 # Application definition
